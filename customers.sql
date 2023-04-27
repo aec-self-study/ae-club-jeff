dee2b1c5cb4ebaa1ec40e9ba4bc35327c2c202a7
Jeffrey Ekeanyanwu <90063850+jeffekeanyanwu@users.noreply.github.com>
2023-04-27 19:03:53 +0000

    added sql file for pr

---
+++ customers.sql
@@ -0,0 +1,9 @@
+select 
+distinct(o.customer_id), 
+c.name, 
+c.email, 
+first_value(created_at) over (partition by customer_id order by created_at asc) as first_order_at, 
+count(total) as number_of_orders from `analytics-engineers-club.coffee_shop.orders` o
+join `analytics-engineers-club.coffee_shop.customers` c on c.id = o.customer_id
+group by 1,2,3, created_at
+order by first_order_at limit 5
